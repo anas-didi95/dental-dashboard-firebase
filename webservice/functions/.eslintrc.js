a9246f09abca6693df1fd0fa5bd606673dc11cfe
Anas Juwaidi <anas.didi95@gmail.com>
2023-05-30 20:55:34 +0800

    FIX: Fix linter conflicts

--- webservice/functions/.eslintrc.js
+++ webservice/functions/.eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     "plugin:import/typescript",
     "google",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -26,8 +27,8 @@ module.exports = {
     "import",
   ],
   rules: {
-    "quotes": ["error", "double"],
+    // "quotes": ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    // "indent": ["error", 2],
   },
 };
