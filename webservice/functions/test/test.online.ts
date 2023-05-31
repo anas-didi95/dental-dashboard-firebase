5d8812a903143367cb11cb9629abae57c99b6004
Anas Juwaidi <anas.didi95@gmail.com>
2023-05-31 21:47:03 +0800

    ADD: Add sample unit test firebase functions

--- webservice/functions/test/test.online.ts
+++ webservice/functions/test/test.online.ts
@@ -1,6 +1,9 @@
 import "mocha";
 import { expect } from "chai";
+import * as fft from "firebase-functions-test"
+import { AppOptions } from "firebase-admin"
 
+const test2 = fft(import("../config/dental-dashboard-firebase-config") as AppOptions, "../config/dental-dashboard-firebase-adminsdk-8zp2m-fdcd7ad194.json")
 const hello = () => "HELLO WORLD";
 
 describe("Hello function", () => {
@@ -9,3 +12,9 @@ describe("Hello function", () => {
     expect(result).to.equal("HELLO WORLD");
   });
 });
+
+describe("Cloud function", () => {
+  after(() => {
+    test2.cleanup()
+  })
+})
