acb3b7634c8b268c20a47a38f35b8c4411ecaa02
Anas Juwaidi <anas.didi95@gmail.com>
2023-06-04 21:10:54 +0800

    ADD: Add express to handle http routing

---
+++ webservice/functions/src/hello-world/index.ts
@@ -0,0 +1,13 @@
+import * as express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello Firebase")
+})
+
+app.get("/:name", (req, res) => {
+  res.send(`Hello ${req.params.name}`)
+})
+
+export default app
