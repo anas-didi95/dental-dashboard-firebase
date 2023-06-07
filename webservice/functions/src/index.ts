7742c85255468c2e7bd099aa3f5d5ddd92067c58
Anas Juwaidi <anas.didi95@gmail.com>
2023-06-07 13:18:34 +0000

    ADD: Add sample graphql handler

--- webservice/functions/src/index.ts
+++ webservice/functions/src/index.ts
@@ -2,3 +2,6 @@ import * as functions from "firebase-functions"
 
 import HelloWorldHandler from "./hello-world"
 export const helloWorld = functions.https.onRequest(HelloWorldHandler)
+
+import GraphQLHandler from "./graphql"
+export const graphql = functions.https.onRequest(GraphQLHandler)
