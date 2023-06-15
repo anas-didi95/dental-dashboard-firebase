4910188a25c1b714cbb1db84ae12daf9339f8ec4
Anas Juwaidi <anas.didi95@gmail.com>
2023-06-15 20:52:23 +0800

    REF: Refactor graphql

--- webservice/functions/src/graphql/index.ts
+++ webservice/functions/src/graphql/index.ts
@@ -1,115 +1,12 @@
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { express } from "../utils/helper";
-import { Firestore, Timestamp } from "firebase-admin/firestore";
-import { TAppointment, TGQLContext } from "../utils/types";
-import { GraphQLScalarType, Kind } from "graphql";
+import { Firestore } from "firebase-admin/firestore";
+import { TGQLContext } from "../utils/types";
 import DataLoader from "dataloader";
+import { typeDefs, resolvers } from "./scheme"
 
 export default (firestore: Firestore) => {
-  // A schema is a collection of type definitions (hence "typeDefs")
-  // that together define the "shape" of queries that are executed against
-  // your data.
-  const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Patient {
-    name: String!
-  }
-
-  type Appointment {
-    patient: Patient!
-    date: Date!
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-    patients: [Patient]!
-    appointments: [Appointment]!
-  }
-
-  scalar Date
-`;
-
-  const dateScalar = new GraphQLScalarType({
-    name: "Date",
-    description: "Date custom scalar type",
-    serialize(value) {
-      if (value instanceof Date) {
-        return value.getTime(); // Convert outgoing Date to integer for JSON
-      } else if (value instanceof Timestamp) {
-        return value.toDate().getTime();
-      }
-      throw Error("GraphQL Date Scalar serializer expected a `Date` object");
-    },
-    parseValue(value) {
-      if (typeof value === "number") {
-        return new Date(value); // Convert incoming integer to Date
-      }
-      throw new Error("GraphQL Date Scalar parser expected a `number`");
-    },
-    parseLiteral(ast) {
-      if (ast.kind === Kind.INT) {
-        // Convert hard-coded AST string to integer and then to Date
-        return new Date(parseInt(ast.value, 10));
-      }
-      // Invalid hard-coded value (not an integer)
-      return null;
-    },
-  });
-
-  const books = [
-    {
-      title: "The Awakening",
-      author: "Kate Chopin",
-    },
-    {
-      title: "City of Glass",
-      author: "Paul Auster",
-    },
-  ];
-
-  // Resolvers define how to fetch the types defined in your schema.
-  // This resolver retrieves books from the "books" array above.
-  const resolvers = {
-    Date: dateScalar,
-    Query: {
-      books: async () => books,
-      patients: async () => {
-        const resultList = await
-          firestore
-            .collection("patients")
-            .get();
-        return resultList.docs.map((o) => o.data());
-      },
-      appointments: async () => {
-        const resultList = await
-          firestore
-            .collection("appointments")
-            .get();
-        return resultList.docs.map((o) => o.data());
-      },
-    },
-    Appointment: {
-      patient: async (
-        parent: TAppointment,
-        _: unknown,
-        context: TGQLContext
-      ) => {
-        return context.patientLoader.load(parent.patientId);
-      },
-    },
-  };
-
   const context: TGQLContext = {
     patientLoader: new DataLoader(async (keys) => {
       return keys.map(async (key) => {
@@ -128,7 +25,7 @@ export default (firestore: Firestore) => {
   // definition and your set of resolvers.
   const server = new ApolloServer({
     typeDefs,
-    resolvers,
+    resolvers: resolvers(firestore)
   });
 
   const app = express();
---
+++ webservice/functions/src/graphql/scheme.ts
@@ -0,0 +1,87 @@
+import { Firestore, Timestamp } from "firebase-admin/firestore";
+import { GraphQLScalarType, Kind } from "graphql"
+import { TAppointment, TGQLContext } from "../utils/types"
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  scalar Date
+
+  type Patient {
+    name: String!
+  }
+
+  type Appointment {
+    patient: Patient!
+    date: Date!
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    patients: [Patient]!
+    appointments: [Appointment]!
+  }
+`;
+
+// Resolvers define how to fetch the types defined in your schema.
+const resolvers = (firestore: Firestore) => ({
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: "Date custom scalar type",
+    serialize(value) {
+      if (value instanceof Date) {
+        return value.getTime(); // Convert outgoing Date to integer for JSON
+      } else if (value instanceof Timestamp) {
+        return value.toDate().getTime();
+      }
+      throw Error("GraphQL Date Scalar serializer expected a `Date` object");
+    },
+    parseValue(value) {
+      if (typeof value === "number") {
+        return new Date(value); // Convert incoming integer to Date
+      }
+      throw new Error("GraphQL Date Scalar parser expected a `number`");
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        // Convert hard-coded AST string to integer and then to Date
+        return new Date(parseInt(ast.value, 10));
+      }
+      // Invalid hard-coded value (not an integer)
+      return null;
+    },
+  }),
+  Query: {
+    patients: async () => {
+      const resultList = await
+        firestore
+          .collection("patients")
+          .get();
+      return resultList.docs.map((o) => o.data());
+    },
+    appointments: async () => {
+      const resultList = await
+        firestore
+          .collection("appointments")
+          .get();
+      return resultList.docs.map((o) => o.data());
+    },
+  },
+  Appointment: {
+    patient: async (
+      parent: TAppointment,
+      _: unknown,
+      context: TGQLContext
+    ) => {
+      return context.patientLoader.load(parent.patientId);
+    },
+  },
+}
+);
+
+export { typeDefs, resolvers }
