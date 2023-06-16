1772d835055d66175029e4a77181ffd1f8491b0f
Anas Juwaidi <anas.didi95@gmail.com>
2023-06-16 20:46:47 +0800

    ADD: Add handler to save deploy status

--- webservice/functions/src/index.ts
+++ webservice/functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions"
+import * as functions from "firebase-functions/v2"
 import firebase from "firebase-admin"
 
 const firebaseApp = firebase.initializeApp()
@@ -8,3 +8,13 @@ export const helloWorld = functions.https.onRequest(helloWorldHandler())
 
 import graphQLHandler from "./graphql"
 export const graphql = functions.https.onRequest(graphQLHandler(firebaseApp.firestore()))
+
+const healthData = {
+  deployDate: firebase.firestore.Timestamp.now(),
+  isOnline: true
+}
+const doc = firebaseApp.firestore().collection("server").doc("health")
+doc.get().then(record => {
+  if (record.exists) return doc.update(healthData)
+  return doc.create(healthData)
+}).then(result => console.log("[init] Functions deployed.", { ...healthData, ...result }))
